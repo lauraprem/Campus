d6f6c94a85cd9ae1cf8fe8c318768c65162f904e
FaCoInf <corinne.fagno@gmail.com>
2014-04-23 01:14:56 +0200

    Modification Accueil

--- Creat3D/Vue/Accueil/index.php
+++ Creat3D/Vue/Accueil/index.php
@@ -3,13 +3,13 @@
   <?php include_once("/Vue/_Commun/menuNavigation.php") ?>
   <div class="col-md-10">
     <div class="row">
-      <div class="col-md-5">
-        <h1>Bienvenue chez Creat3D !</h1>
+      <div class="col-md-7">
+          <h1>Bienvenue chez <span class="text-logo">Creat3D !</span></h1>
         Quel que soit votre besoin en maquette architecturale, nous pouvons vous aider !
-        Nous imprimons en 3D vos modélisations en tailles variées et en couleurs. <br><br>Pour en savoir plus sur les tailles, matériaux et coulurs que nous proposons, rendez-vous sur "Notre offre".
+        Nous imprimons en 3D vos modélisations en tailles variées et en couleurs. <br><br>Pour en savoir plus sur les tailles, matériaux et couleurs que nous proposons, rendez-vous sur "Notre offre".
         Si vous voulez savoir quelles technologies et savoir-faires nous utilisons, cliquez sur "Notre métier". Pour mieux nous connaitres, allez voir la section "à propos".
       </div>
-      <div class="col-md-5">
+      <div class="col-md-4">
         <figure>
           <img class="img-responsive" src="Contenu/Images/logo.jpg" alt="Copyright all reserved" />
           <figcaption><!-- Legende associe --></figcaption>
