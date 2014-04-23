97c2e663d681375950b1ba024576156f03854602
4lexandre <4lexandre@4lexandre-PC>
2014-04-23 11:12:21 +0200

    quelquestrucs

--- Creat3D/Vue/NotreOffre/index.php
+++ Creat3D/Vue/NotreOffre/index.php
@@ -4,21 +4,22 @@
     <div class="col-md-10">
         <div class="row">
 
-                <h1> Imprimer une maquette 3D, c'est facile!</h1><br><br>
+                <h1>Imprimer une maquette 3D, c'est facile!</h1>
 
-                <h3>Etape 1 : je choisi mon matériau<br> </h3>
+                <h3>&Eacute;tape 1 : je choisi mon matériau</h3>
                 <img class="img-responsive" src="Contenu/Images/materiau.bmp" alt="Copyright all reserved" style ="max-height: 350px" />
 
-               <h3> Etape 2 : je choisi ma taille<br><br></h3>
-                exemples: <br>
+               <h3>&Eacute;tape 2 : je choisi ma taille</h3>
+                <p>Exemples : <br>
                 35x35x20<br>
                 70x70x40<br>
-                105x105x60<br><br><br>
+                105x105x60</p>
 
-               <h3> Etape 3 : je choisi mes finitions<br><br></h3>
+               <h3>&Eacute;tape 3 : je choisi mes finitions</h3>
+                <p>Exemples : <br>
                 polissage <br>
                 peinture<br>
-                autre ...<br><br><br>
+                autre ...</p>
 
 
                 <h3 class="text-center"> Pour obtenir un devis de votre maquette,<br> veuillez remplir le formulaire sur la page <a href = "nousContacter" >"nous contacter" </a> avant de nous l'envoyer</h3>
