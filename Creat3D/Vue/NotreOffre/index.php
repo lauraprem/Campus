a973d278cb1d29176f636bb7220023df64c99ff7
lauraprem <premillieu.laura@gmail.com>
2014-04-23 10:42:26 +0200

    notre offre

--- Creat3D/Vue/NotreOffre/index.php
+++ Creat3D/Vue/NotreOffre/index.php
@@ -1,4 +1,30 @@
-<?php $this->titre = "Notre Offre";?>
+<?php $this->titre = "Notre Offre"; ?>
 <div class="row">
-  <?php include_once("/Vue/_Commun/menuNavigation.php") ?>
+    <?php include_once("/Vue/_Commun/menuNavigation.php") ?>
+    <div class="col-md-10">
+        <div class="row">
+            <div class="col-md-7">
+
+                Imprimer une maquette 3D, c'est facile!<br><br>
+
+                Etape 1 : je choisi mon matériau<br> 
+                <img class="img-responsive" src="Contenu/Images/materiau.bmp" alt="Copyright all reserved" style ="max-height: 270px" />
+
+                Etape 2 : je choisi ma taille<br><br>
+                exemples: <br>
+                35x35x20<br>
+                70x70x40<br>
+                105x105x60<br><br><br>
+
+                Etape 3 : je choisi mes finitions<br><br>
+                polissage <br>
+                peinture<br>
+                autre ...<br<br><br>
+
+
+                Pour obtenir un devis de votre maquette, veuillez remplir le formulaire sur la page "nous contacter"  avant de nous l'envoyer
+
+            </div>
+        </div>
+    </div>
 </div>
